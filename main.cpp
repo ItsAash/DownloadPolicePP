d76d0acd984493a76a2fbca9c958d8e68e6521aa
Diwas Parajuli <parajuli.diwas@gmail.com>
2022-09-07 15:09:37 +0545

    styles sheeet

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
-
-#include <QApplication>
+#include <QFile>
+/*#include <QApplication>*/
+#include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+
+    QFile styleSheetFile("./style.qss");
+    styleSheetFile.open(QFile :: ReadOnly);
+    QString stylesheet = QLatin1String(styleSheetFile.readAll());
+    app.setStyleSheet(stylesheet);
+
     MainWindow w;
     w.show();
-    return a.exec();
+    return app.exec();
 }
