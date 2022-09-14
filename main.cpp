771563567f7d78d877c445c946f7be37b47398fd
Ashish Neupane <ashishneupane125@gmail.com>
2022-09-14 14:53:14 +0545

    Add stylesheet

--- main.cpp
+++ main.cpp
@@ -7,12 +7,14 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QFile styleSheetFile("./style.qss");
-    styleSheetFile.open(QFile :: ReadOnly);
+    QFile styleSheetFile(":/qss/style.qss");
+    styleSheetFile.open(QFile::ReadOnly);
+
+    // Apply the loaded stylesheet
     QString stylesheet = QLatin1String(styleSheetFile.readAll());
     app.setStyleSheet(stylesheet);
-
     MainWindow w;
     w.show();
+    w.setWindowTitle("DownloadPolice++");
     return app.exec();
 }
